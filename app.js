429739862da3893bb35125614a4021a0ddabc025
Andre Jia <ajia.117@gmail.com>
2015-10-10 11:49:18 -0700

    Implemented new player connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express"),
-    http = require("http"),
-    io = require("socket.io");
+    app = express(),
+    server = require("http").Server(app),
+    io = require("socket.io")(server);
 var bodyParser = require("body-parser");
 var MongoClient = require("mongodb").MongoClient;
 
@@ -15,18 +16,44 @@ MongoClient.connect("mongodb://localhost:27017/fugitive", function(err, db) {
 
 
 
-var app = express(),
-    server = http.Server(app),
-    socket = io(server);
-
 app.use(bodyParser.json()); // for parsing application/json
+
+
+function add_player_to_room(pname, rname) {
+  var player =
+  {
+    "name": pname,
+    "class": null,
+    "rstat": false
+  }
+  RoomsCollection.findOne({'name': rname}, {w:1}, function(err, item) {
+    if(!err) {
+      item.players.push(player);
+      RoomsCollection.updateOne(
+        {'name': rname},
+        {$set:{'players': item.players}},
+        function(err, results) {
+          console.log(results);
+    });
+    }
+  })
+}
+
+io.sockets.on('connect', function(socket){
+    socket.on('join room', function(rname, pname) {
+        console.log('Player %s joins room %s.', pnam, rname);
+        add_player_to_room(pname, rname);
+        socket.join(rname); 
+    })
+});
+
 
 app.post("/create_room", function (req, res) {
   var name = req.body.name;
   var time = req.body.time;
 
   if(RoomsCollection != null) {
-    var room = {'name': name, 'time': time};
+    var room = {'name': name, 'time': time, 'players': []};
     RoomsCollection.insert(room, {w:1}, function(err, result) {
       if(!err) {
         res.send('1');
