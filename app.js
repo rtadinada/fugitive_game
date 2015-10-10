0a0937df4acdbc241e57396b8f127c84af553282
Andre Jia <ajia.117@gmail.com>
2015-10-10 14:29:26 -0700

    Stage one complete - room built

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ function add_player_to_room(pname, rname) {
   var player =
   {
     "name": pname,
-    "class": null,
-    "rstat": false
+    "team": -1,
+    "ready_status": false
   }
   RoomsCollection.findOne({'name': rname}, {w:1}, function(err, item) {
     if(!err) {
@@ -34,17 +34,89 @@ function add_player_to_room(pname, rname) {
         {$set:{'players': item.players}},
         function(err, results) {
           console.log(results);
-    });
+          item.players.forEach(function (entry) {
+            player_update(entry.name, rname);
+          });
+      });
     }
   })
 }
 
+function team_selection(player, room, team) {
+  RoomsCollection.findOne({'name': room}, {w:1}, function(err, item) {
+    if(!err) {
+      item.players[item.players.find({'name': player})].team = team;
+      RoomsCollection.updateOne(
+        {'name': room},
+        {$set:{'players': item.players}},
+        function(err, results) {
+          console.log(results);
+          player_update(player, room);
+      });
+    }
+  });
+}
+
+function team_locked(player, room) {
+  RoomsCollection.findOne({'name': room}, {w:1}, function(err, item) {
+    if(!err) {
+      item.players[item.players.find({'name': player})].ready_status =
+        true;
+      RoomsCollection.updateOne(
+        {'name': room},
+        {$set:{'players': item.players}},
+        function(err, results) {
+          console.log(results);
+          player_update(player, room);
+          var allReady = true;
+          var firstRobber = "";
+          item.players.forEach(function (entry) {
+            if(!entry.ready_status) {
+              allReady = false;
+            }
+            if(entry.team == 0) {
+              firstRobber = entry.name;
+            }
+          });
+          if(allReady) {
+            io.sockets.to(room).emit('start select locations', firstRobber);
+          }
+      });
+    }
+  });
+}
+
+function player_update(pname, rname) {
+  RoomsCollection.findOne({'name': rname}, {w:1}, function(err, item) {
+    if(!err) {
+      var player =
+        item.players.filter(function(x) { return x.name === pname})[0];
+      io.sockets.to(rname).emit('player update', player.name, player.team, player.ready_status);
+    }
+  });
+}
+
 io.sockets.on('connect', function(socket){
+    var player = "";
+    var room = "";
+
     socket.on('join room', function(rname, pname) {
-        console.log('Player %s joins room %s.', pnam, rname);
-        add_player_to_room(pname, rname);
-        socket.join(rname); 
-    })
+        player = pname; //HUGE HACK TODO: FIX PLS RAVI WHY
+        room = rname;
+        console.log('Player %s joins room %s.', player, room);
+        socket.join(room);
+        add_player_to_room(player, room);
+    });
+
+    socket.on('team selection', function(team) {
+      console.log('Player %s joins team %d', player, team);
+      team_selection(player, room, team);
+    });
+
+    socket.on('team locked', function() {
+      console.log('Player %s confirms team', player);
+      team_locked(player, room);
+    });
 });
 
 
